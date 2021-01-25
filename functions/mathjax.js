125f7ee44d87debbd81d2df1fc8a74c888d79a1e
Hanai <ihanai1991@gmail.com>
2021-01-25 19:35:08 +0800

    add mathjax

---
+++ functions/mathjax.js
@@ -0,0 +1,32 @@
+var mjAPI = require("mathjax-node");
+
+mjAPI.config({
+  MathJax: {
+  }
+});
+
+mjAPI.start();
+
+const renderSVG = (formula) => {
+  return new Promise((resolve, reject) => {
+    mjAPI.typeset({
+      math: formula,
+      format: "TeX", // or "inline-TeX", "MathML"
+      svg: true,
+    }, function (data) {
+      if (!data.errors) {
+        resolve(data);
+      }
+    });
+  });
+}
+
+exports.handler = async function (event) {
+  const svg = await renderSVG('E = mc^2');
+  return {
+    statusCode: 200,
+    body: svg
+  };
+};
+
+
