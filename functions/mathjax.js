99380700bd7862ac89df68072ba5ad126642c48e
Hanai <ihanai1991@gmail.com>
2021-01-25 21:56:25 +0800

    request handler

--- functions/mathjax.js
+++ functions/mathjax.js
@@ -1,32 +1,43 @@
 var mjAPI = require("mathjax-node");
 
 mjAPI.config({
-  MathJax: {
-  }
+  MathJax: {},
 });
 
 mjAPI.start();
 
 const renderSVG = (formula) => {
   return new Promise((resolve, reject) => {
-    mjAPI.typeset({
-      math: formula,
-      format: "TeX", // or "inline-TeX", "MathML"
-      svg: true,
-    }, function (data) {
-      if (!data.errors) {
-        resolve(data.svg);
+    mjAPI.typeset(
+      {
+        math: formula,
+        format: "TeX", // or "inline-TeX", "MathML"
+        svg: true,
+      },
+      function (data) {
+        if (!data.errors) {
+          resolve(data.svg);
+        }
       }
-    });
+    );
   });
-}
-
-exports.handler = async function (event) {
-  const svg = await renderSVG('E = mc^2');
-  return {
-    statusCode: 200,
-    body: svg
-  };
 };
 
-
+exports.handler = async function (event) {
+  const { queryStringParameters, httpMethod } = event;
+  if (
+    httpMethod.toUpperCase() === "GET" &&
+    queryStringParameters &&
+    typeof queryStringParameters.tex === "string"
+  ) {
+    const formula = decodeURIComponent(queryStringParameters.tex);
+    const svg = await renderSVG(formula);
+    return {
+      headers: {
+        "content-type": "image/svg+xml",
+      },
+      statusCode: 200,
+      body: svg,
+    };
+  }
+};
